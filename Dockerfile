0bafc84b73afb54e5513d57286b574d13665378c
Victor Palma <palma.victor@gmail.com>
2016-04-26 13:43:00 -0500

    minor fixes

--- Dockerfile
+++ Dockerfile
@@ -1,32 +1,32 @@
 FROM anapsix/alpine-java:jdk8
 MAINTAINER Victor Palma <palma.victor@gmail.com>
 
-ENV KAFKA_VERSION 0.9.0.1 \
-    KAFKA_USER kafka \
-    KAFKA_GROUP kafka \
-    KAFKA_HOME /opt/kafka \
-    KAFKA_DIR /var/lib/kafka \
-    JMX_PORT 19092 \
-    SCALA_2_11_URL http://mirrors.gigenet.com/apache/kafka/${KAFKA_VERSION}/kafka_2.11-${KAFKA_VERSION}.tgz \
-    SCALA_2_10_URL http://mirrors.gigenet.com/apache/kafka/${KAFKA_VERSION}/kafka_2.10-${KAFKA_VERSION}.tgz
+ENV KAFKA_VERSION="0.9.0.1" \
+    KAFKA_USER="kafka" \
+    KAFKA_GROUP="kafka" \
+    KAFKA_HOME="/opt/kafka" \
+    KAFKA_LOGS="/opt/kafka/logs" \
+    KAFKA_JKS_HOME="/opt/jks" \
+    KAFKA_DIR="/var/lib/kafka" 
 
-COPY kafka.sh /usr/local/bin/kafka.sh
 
-RUN apk --no-cache add curl tar &&\
-    mkdir -p ${KAFKA_DIR}/log /opt &&\
-    [[ ${KAFKA_VERSION} = "0.9"* ]] && curl -sSL ${SCALA_2_11_URL} | tar zxf - -C /opt || curl -sSL ${SCALA_2_11_URL} | tar zxf - -C /opt &&\
-    ln -s /opt/kafka_2.*-${KAFKA_VERSION} ${KAFKA_HOME} &&\
-    addgroup $KAFKA_GROUP &&\
-    adduser -h ${KAFKA_DIR} -D -s /bin/bash -G ${KAFKA_GROUP} ${KAFKA_USER} &&\
-    chown -R ${KAFKA_USER}:${KAFKA_GROUP} ${KAFKA_DIR} ${KAFKA_HOME}/ &&\
-    chmod +x /usr/local/bin/kafka.sh
+RUN apk --no-cache add curl tar wget && \
+    mkdir -p ${KAFKA_DIR}/log /opt && \
+    wget -q -O - http://mirrors.gigenet.com/apache/kafka/${KAFKA_VERSION}/kafka_2.11-${KAFKA_VERSION}.tgz | tar -xzf - -C /opt  && \
+    mv /opt/kafka_2.11-${KAFKA_VERSION} ${KAFKA_HOME} && \
+    addgroup $KAFKA_GROUP && \
+    adduser -h ${KAFKA_DIR} -D -s /bin/bash -G ${KAFKA_GROUP} ${KAFKA_USER} && \
+    chown -R ${KAFKA_USER}:${KAFKA_GROUP} ${KAFKA_DIR} ${KAFKA_HOME}/
 
 USER ${KAFKA_USER}
 
+WORKDIR ${KAFKA_HOME}
+
+COPY start-kafka.sh ${KAFKA_HOME}/bin/start-kafka.sh
+
 # Expose client port (9092/tcp)
-EXPOSE 9092 ${JMX_PORT}
+EXPOSE 9092 19092
 
-VOLUME ["${KAFKA_DIR}"]
+VOLUME ["${KAFKA_DIR}", "${KAFKA_JKS_HOME}", "${KAFKA_LOGS}"]
 
-ENTRYPOINT ["/usr/local/bin/kafka.sh"]
-CMD [""]
+ENTRYPOINT ["/bin/bash", "-xe", "${KAFKA_HOME}/bin/start-kafka.sh"]
---
+++ start-kafka.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+
+# Optional ENV variables:
+# * ADVERTISED_HOST: the external ip for the container, e.g. `docker-machine ip \`docker-machine active\``
+# * ADVERTISED_PORT: the external port for Kafka, e.g. 9092
+# * ZK_CHROOT: the zookeeper chroot that's used by Kafka (without / prefix), e.g. "kafka"
+# * LOG_RETENTION_HOURS: the minimum age of a log file in hours to be eligible for deletion (default is 168, for 1 week)
+# * LOG_RETENTION_BYTES: configure the size at which segments are pruned from the log, (default is 1073741824, for 1GB)
+# * NUM_PARTITIONS: configure the default number of log partitions per topic
+
+function add_config_param {
+    echo "$1: $2"
+    if grep -q $1 $KAFKA_HOME/config/server.properties; then
+        sed -r -i "s|($1)=(.*)|\1=$2|g" $KAFKA_HOME/config/server.properties
+    else
+        echo "$1=$2" >> $KAFKA_HOME/config/server.properties
+    fi
+}
+
+# Set the external host and port
+if [ ! -z "$ADVERTISED_HOST" ]; then
+    echo "advertised host: $ADVERTISED_HOST"
+    sed -r -i "s/#(advertised.host.name)=(.*)/\1=$ADVERTISED_HOST/g" $KAFKA_HOME/config/server.properties
+fi
+if [ ! -z "$ADVERTISED_PORT" ]; then
+    add_config_param "port" $ADVERTISED_PORT
+    echo "advertised port: $ADVERTISED_PORT"
+    sed -r -i "s/#(advertised.port)=(.*)/\1=$ADVERTISED_PORT/g" $KAFKA_HOME/config/server.properties
+fi
+
+# Set the log directory for kafka
+if [ ! -z "$KAFKA_LOG_DIR" ]; then
+    add_config_param "log.dirs" ${KAFKA_LOG_DIR}
+    echo "kafka log directory: ${KAFKA_LOG_DIR}"
+    sed -r -i "s/(log.dirs)=(.*)/\1=${KAFKA_LOG_DIR}/g" $KAFKA_HOME/config/server.properties
+fi
+
+# Set the zookeeper
+if [ ! -z "${ZK_HOST}" ]; then
+    # configure kafka
+    sed -r -i "s/(zookeeper.connect)=(.*)/\1=${ZK_HOST}:${ZK_PORT}\/kafka/g" $KAFKA_HOME/config/server.properties
+fi
+
+# Allow specification of log retention policies
+if [ ! -z "$LOG_RETENTION_HOURS" ]; then
+    echo "log retention hours: $LOG_RETENTION_HOURS"
+    sed -r -i "s/(log.retention.hours)=(.*)/\1=$LOG_RETENTION_HOURS/g" $KAFKA_HOME/config/server.properties
+fi
+if [ ! -z "$LOG_RETENTION_BYTES" ]; then
+    echo "log retention bytes: $LOG_RETENTION_BYTES"
+    sed -r -i "s/#(log.retention.bytes)=(.*)/\1=$LOG_RETENTION_BYTES/g" $KAFKA_HOME/config/server.properties
+fi
+
+# Configure the default number of log partitions per topic
+if [ ! -z "$NUM_PARTITIONS" ]; then
+    echo "default number of partition: $NUM_PARTITIONS"
+    sed -r -i "s/(num.partitions)=(.*)/\1=$NUM_PARTITIONS/g" $KAFKA_HOME/config/server.properties
+fi
+
+# Enable/disable auto creation of topics
+if [ ! -z "$AUTO_CREATE_TOPICS" ]; then
+    echo "auto.create.topics.enable: $AUTO_CREATE_TOPICS"
+    echo "auto.create.topics.enable=$AUTO_CREATE_TOPICS" >> $KAFKA_HOME/config/server.properties
+fi
+
+## SSL
+add_config_param "security.inter.broker.protocol" "SSL"
+add_config_param "ssl.enabled.protocols" "TLSv1.2,TLSv1.1,TLSv1"
+
+if [ ! -z "$SUPER_USERS" ]; then
+    add_config_param "super.users" $SUPER_USERS
+fi
+
+add_config_param "listeners" "SSL://:$ADVERTISED_PORT"
+add_config_param "advertised.listeners" "SSL://$ADVERTISED_HOST:$ADVERTISED_PORT"
+
+# Configure SSL Location
+if [ ! -z "$SSL_KEYSTORE_LOCATION" ]; then
+    add_config_param "ssl.keystore.location" ${SSL_KEYSTORE_LOCATION}
+    add_config_param "ssl.keystore.password" ${SSL_KEYSTORE_PASSWORD}
+fi
+
+# Configure SSL Truststore
+if [ ! -z "$SSL_TRUSTSTORE_LOCATION" ]; then
+    add_config_param "ssl.truststore.location" ${SSL_TRUSTSTORE_LOCATION}
+    add_config_param "ssl.truststore.password" ${SSL_TRUSTSTORE_PASSWORD}
+fi
+
+# Configure auth
+if [ ! -z "$SSL_CLIENT_AUTH" ]; then
+    add_config_param "ssl.client.auth" $SSL_CLIENT_AUTH
+    add_config_param "authorizer.class.name" "kafka.security.auth.SimpleAclAuthorizer"
+
+    sed -r -i "s|(log4j.logger.kafka.authorizer.logger)=(.*)|\1=DEBUG, authorizerAppender|g" $KAFKA_HOME/config/log4j.properties
+fi
+
+# Run Kafka
+$KAFKA_HOME/bin/kafka-server-start.sh $KAFKA_HOME/config/server.properties
