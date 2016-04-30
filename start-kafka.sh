3a8df1ab9d4e459381e7122a632a367c9f8b6b7c
Victor Palma <palma.victor@gmail.com>
2016-04-30 09:44:15 -0500

    update advertised.listeners

--- start-kafka.sh
+++ start-kafka.sh
@@ -9,101 +9,101 @@
 # * NUM_PARTITIONS: configure the default number of log partitions per topic
 
 function add_config_param {
-    echo "$1: $2"
-    if grep -q $1 $KAFKA_HOME/config/server.properties; then
-        sed -r -i "s|($1)=(.*)|\1=$2|g" $KAFKA_HOME/config/server.properties
+    echo "${1}: ${2}"
+    if grep -q ${1} ${KAFKA_HOME}/config/server.properties; then
+        sed -r -i "s|(${1})=(.*)|\1=${2}|g" ${KAFKA_HOME}/config/server.properties
     else
-        echo "$1=$2" >> $KAFKA_HOME/config/server.properties
+        echo "${1}=${2}" >> ${KAFKA_HOME}/config/server.properties
     fi
 }
 
 # Set the external host and port
-if [ ! -z "$ADVERTISED_HOST" ]; then
-    echo "advertised host: $ADVERTISED_HOST"
-    sed -r -i "s/#(advertised.host.name)=(.*)/\1=$ADVERTISED_HOST/g" $KAFKA_HOME/config/server.properties
+if [ ! -z "${ADVERTISED_HOST}" ]; then
+    echo "advertised host: ${ADVERTISED_HOST}"
+    sed -r -i "s/#(advertised.host.name)=(.*)/\1=${ADVERTISED_HOST}/g" ${KAFKA_HOME}/config/server.properties
 fi
-if [ ! -z "$ADVERTISED_PORT" ]; then
-    add_config_param "port" $ADVERTISED_PORT
-    echo "advertised port: $ADVERTISED_PORT"
-    sed -r -i "s/#(advertised.port)=(.*)/\1=$ADVERTISED_PORT/g" $KAFKA_HOME/config/server.properties
+if [ ! -z "${ADVERTISED_PORT}" ]; then
+    add_config_param "port" ${ADVERTISED_PORT}
+    echo "advertised port: ${ADVERTISED_PORT}"
+    sed -r -i "s/#(advertised.port)=(.*)/\1=${ADVERTISED_PORT}/g" ${KAFKA_HOME}/config/server.properties
 fi
 
 # Set the log directory for kafka
-if [ ! -z "$KAFKA_LOG_DIR" ]; then
+if [ ! -z "${KAFKA_LOG_DIR}" ]; then
     add_config_param "log.dirs" ${KAFKA_LOG_DIR}
     echo "kafka log directory: ${KAFKA_LOG_DIR}"
-    sed -r -i "s/(log.dirs)=(.*)/\1=-1/g" $KAFKA_HOME/config/server.properties
+    sed -r -i "s/(log.dirs)=(.*)/\1=-1/g" ${KAFKA_HOME}/config/server.properties
 fi
 
 # Set the broker id
-if [ ! -z "$KAFKA_BROKER_ID" ]; then
+if [ ! -z "${KAFKA_BROKER_ID}" ]; then
     add_config_param "broker.id" "-1"
     echo "kafka broker id set to auto generate: -1"
-    sed -r -i "s/(broker.id)=(.*)/\1=${KAFKA_BROKER_ID}/g" $KAFKA_HOME/config/server.properties
+    sed -r -i "s/(broker.id)=(.*)/\1=${KAFKA_BROKER_ID}/g" ${KAFKA_HOME}/config/server.properties
 else
     add_config_param "broker.id" "${KAFKA_BROKER_ID}"
     echo "kafka broker id set to: ${KAFKA_BROKER_ID}"
-    sed -r -i "s/(broker.id)=(.*)/\1=${KAFKA_BROKER_ID}/g" $KAFKA_HOME/config/server.properties
+    sed -r -i "s/(broker.id)=(.*)/\1=${KAFKA_BROKER_ID}/g" ${KAFKA_HOME}/config/server.properties
 fi
 
 # Set the zookeeper
 if [ ! -z "${ZK_HOST}" ]; then
     # configure kafka
-    sed -r -i "s/(zookeeper.connect)=(.*)/\1=${ZK_HOST}:${ZK_PORT}\/kafka/g" $KAFKA_HOME/config/server.properties
+    sed -r -i "s/(zookeeper.connect)=(.*)/\1=${ZK_HOST}:${ZK_PORT}\/kafka/g" ${KAFKA_HOME}/config/server.properties
 fi
 
 # Allow specification of log retention policies
-if [ ! -z "$LOG_RETENTION_HOURS" ]; then
-    echo "log retention hours: $LOG_RETENTION_HOURS"
-    sed -r -i "s/(log.retention.hours)=(.*)/\1=$LOG_RETENTION_HOURS/g" $KAFKA_HOME/config/server.properties
+if [ ! -z "${LOG_RETENTION_HOURS}" ]; then
+    echo "log retention hours: ${LOG_RETENTION_HOURS}"
+    sed -r -i "s/(log.retention.hours)=(.*)/\1=${LOG_RETENTION_HOURS}/g" ${KAFKA_HOME}/config/server.properties
 fi
-if [ ! -z "$LOG_RETENTION_BYTES" ]; then
-    echo "log retention bytes: $LOG_RETENTION_BYTES"
-    sed -r -i "s/#(log.retention.bytes)=(.*)/\1=$LOG_RETENTION_BYTES/g" $KAFKA_HOME/config/server.properties
+if [ ! -z "${LOG_RETENTION_BYTES}" ]; then
+    echo "log retention bytes: ${LOG_RETENTION_BYTES}"
+    sed -r -i "s/#(log.retention.bytes)=(.*)/\1=${LOG_RETENTION_BYTES}/g" ${KAFKA_HOME}/config/server.properties
 fi
 
 # Configure the default number of log partitions per topic
-if [ ! -z "$NUM_PARTITIONS" ]; then
-    echo "default number of partition: $NUM_PARTITIONS"
-    sed -r -i "s/(num.partitions)=(.*)/\1=$NUM_PARTITIONS/g" $KAFKA_HOME/config/server.properties
+if [ ! -z "${NUM_PARTITIONS}" ]; then
+    echo "default number of partition: ${NUM_PARTITIONS}"
+    sed -r -i "s/(num.partitions)=(.*)/\1=${NUM_PARTITIONS}/g" ${KAFKA_HOME}/config/server.properties
 fi
 
 # Enable/disable auto creation of topics
-if [ ! -z "$AUTO_CREATE_TOPICS" ]; then
-    echo "auto.create.topics.enable: $AUTO_CREATE_TOPICS"
-    echo "auto.create.topics.enable=$AUTO_CREATE_TOPICS" >> $KAFKA_HOME/config/server.properties
+if [ ! -z "${AUTO_CREATE_TOPICS}" ]; then
+    echo "auto.create.topics.enable: ${AUTO_CREATE_TOPICS}"
+    echo "auto.create.topics.enable=${AUTO_CREATE_TOPICS}" >> ${KAFKA_HOME}/config/server.properties
 fi
 
 ## SSL
 add_config_param "security.inter.broker.protocol" "SSL"
 add_config_param "ssl.enabled.protocols" "TLSv1.2,TLSv1.1,TLSv1"
 
-if [ ! -z "$SUPER_USERS" ]; then
-    add_config_param "super.users" $SUPER_USERS
+if [ ! -z "${SUPER_USERS}" ]; then
+    add_config_param "super.users" ${SUPER_USERS}
 fi
 
-add_config_param "listeners" "SSL://:$ADVERTISED_PORT"
-add_config_param "advertised.listeners" "SSL://$ADVERTISED_HOST:$ADVERTISED_PORT"
+add_config_param "listeners" "PLAINTEXT://:${ADVERTISED_PORT},SSL://:${ADVERTISED_SSL_PORT}"
+add_config_param "advertised.listeners" "PLAINTEXT://${ADVERTISED_HOST}:${ADVERTISED_PORT},SSL://${ADVERTISED_HOST}:${ADVERTISED_SSL_PORT}"
 
 # Configure SSL Location
-if [ ! -z "$SSL_KEYSTORE_LOCATION" ]; then
+if [ ! -z "${SSL_KEYSTORE_LOCATION}" ]; then
     add_config_param "ssl.keystore.location" ${SSL_KEYSTORE_LOCATION}
     add_config_param "ssl.keystore.password" ${SSL_KEYSTORE_PASSWORD}
 fi
 
 # Configure SSL Truststore
-if [ ! -z "$SSL_TRUSTSTORE_LOCATION" ]; then
+if [ ! -z "${SSL_TRUSTSTORE_LOCATION}" ]; then
     add_config_param "ssl.truststore.location" ${SSL_TRUSTSTORE_LOCATION}
     add_config_param "ssl.truststore.password" ${SSL_TRUSTSTORE_PASSWORD}
 fi
 
 # Configure auth
-if [ ! -z "$SSL_CLIENT_AUTH" ]; then
-    add_config_param "ssl.client.auth" $SSL_CLIENT_AUTH
+if [ ! -z "${SSL_CLIENT_AUTH}" ]; then
+    add_config_param "ssl.client.auth" ${SSL_CLIENT_AUTH}
     add_config_param "authorizer.class.name" "kafka.security.auth.SimpleAclAuthorizer"
 
-    sed -r -i "s|(log4j.logger.kafka.authorizer.logger)=(.*)|\1=DEBUG, authorizerAppender|g" $KAFKA_HOME/config/log4j.properties
+    sed -r -i "s|(log4j.logger.kafka.authorizer.logger)=(.*)|\1=DEBUG, authorizerAppender|g" ${KAFKA_HOME}/config/log4j.properties
 fi
 
 # Run Kafka
-$KAFKA_HOME/bin/kafka-server-start.sh $KAFKA_HOME/config/server.properties
+${KAFKA_HOME}/bin/kafka-server-start.sh ${KAFKA_HOME}/config/server.properties
